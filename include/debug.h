47d4bbb0b214b5f5eef27943f2feb0fa17e5ee4f
Sascha Lammers <sascha_lammers@gmx.de>
2026-07-18 00:23:36 -0700

    Serial4 added

--- include/debug.h
+++ include/debug.h
@@ -9,21 +9,18 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
-
-#if DEBUG_OUTPUT == DEBUG_OUTPUT_SWD
-    #include <stm32f1xx.h>
-#endif
+#include <stm32f1xx.h>
 
 // === debug settings ===
 
 #define DEBUG_OUTPUT_NONE       0
-#define DEBUG_OUTPUT_SERIAL1    1
+#define DEBUG_OUTPUT_SERIAL     1
 #define DEBUG_OUTPUT_SERIAL4    2
 #define DEBUG_OUTPUT_SWD        3
 #define DEBUG_OUTPUT_USB        4
 
 #ifndef DEBUG_OUTPUT
-    #define DEBUG_OUTPUT        DEBUG_OUTPUT_SERIAL1
+    #define DEBUG_OUTPUT        DEBUG_OUTPUT_SERIAL
 #endif
 
 #define DEBUG_ERROR             0
@@ -49,13 +46,23 @@ const char *debug_function_name(const char *signature, char *out, size_t outSize
 
     #define DEBUG_PRINT(level, msg, ...) do {} while(0)
 
-#elif DEBUG_OUTPUT == DEBUG_OUTPUT_SERIAL1
+#elif DEBUG_OUTPUT == DEBUG_OUTPUT_SERIAL
 
     #define DEBUG_PRINT(level, msg, ...) \
         do { \
             if (level <= DEBUG_LEVEL) { \
                 char _debug_function[96]; \
                 Serial.printf("[%06lu] %s " msg "\n", millis(), debug_function_name(DEBUG_FUNCTION_SIG, _debug_function, sizeof(_debug_function)), ##__VA_ARGS__); \
+            } \
+        } while(0)
+
+#elif DEBUG_OUTPUT == DEBUG_OUTPUT_SERIAL4
+
+    #define DEBUG_PRINT(level, msg, ...) \
+        do { \
+            if (level <= DEBUG_LEVEL) { \
+                char _debug_function[96]; \
+                Serial4.printf("[%06lu] %s " msg "\n", millis(), debug_function_name(DEBUG_FUNCTION_SIG, _debug_function, sizeof(_debug_function)), ##__VA_ARGS__); \
             } \
         } while(0)
 
